03c12c11b8a0af1bb64a2b4f18ddb0edb6e9e5f7
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-11-21 14:10:27 +0300

    Update digital_clock.c

--- My_C_Projects/digital_clock.c
+++ My_C_Projects/digital_clock.c
@@ -7,12 +7,13 @@ int main(void)
 {
 time_t s, val = 1;
 struct tm *current_time;
-// time in seconds
+/*time in seconds*/
 s = time(NULL);
-// to get current time
+/*to get current time*/
 current_time = localtime(&s);
-// print time in minutes,
-// hours and seconds
+/*print time in minutes,
+*hours and seconds
+*/
 printf("%02d:%02d:%02d",
 current_time->tm_hour,
 current_time->tm_min,
